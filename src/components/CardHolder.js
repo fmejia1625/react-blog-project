379007d0a6491907f659391668902ae4402d343e
Francisco Mejia <frankie@Franciscos-MacBook-Pro.local>
2021-05-19 12:24:47 -0400

    Fixed up landing page

--- src/components/CardHolder.js
+++ src/components/CardHolder.js
@@ -2,6 +2,10 @@ import React from 'react';
 import CardItems from './CardItems';
 import './Cards.css';
 import deskImg from './img/desk.JPG'; 
+import queensImg from './img/queens.jpg';
+import dashImg from './img/canvas.JPG';
+import lectureImg from './img/lecture.JPG'
+import catImg from './img/cat.jpeg'
 
 function CardHolder() {
   return (
@@ -11,10 +15,36 @@ function CardHolder() {
           <div className="cardsWrapper">
             <ul className="cardsItems">
               <CardItems 
-              src = {deskImg}
-              text = "How my programming skill has progressed since Day One. (CLICK ME!)"
-              label = "Coding Setup"
-              path = '/blogposts'
+                src = {deskImg}
+                text = "How my programming skill has progressed since Day One. (CLICK ME!)"
+                label = "Coding Setup"
+                path = '/blogposts'
+              />
+              <CardItems 
+                src = {dashImg}
+                text = "Weeks One through Four: Being thrown into the fire (What is sleep?)"
+                label = "Checking on what assignments are due for the day"
+                path = '/blogposts'
+              />
+            </ul>
+            <ul className="cardsItems">
+              <CardItems 
+                src = {lectureImg}
+                text = "Weeks Five through Nine: Battle of Attrition"
+                label = "My instructor Tory giving a lesson"
+                path = '/blogposts'
+              />
+              <CardItems 
+                src = {queensImg}
+                text = "Want to get in contact? Click to find out how!"
+                label = "Whitestone Bridge, Queens, NYC"
+                path = '/contact'
+              />
+              <CardItems 
+                src = {catImg}
+                text = "Join my non-existant community now by signing up. You won't regret it! (hopefully)"
+                label = "Join us"
+                path = '/signUp'
               />
             </ul>
           </div>
@@ -30,3 +60,4 @@ export default CardHolder
 // 3. Import the Cards stylesheet above in order to apply the styling to its respective component container. 
 // 4. Pass in props within CardItems component. 
 // 5. Had to refactor the way in which images are rendered. I had to import the image above and then pass it within curly brackets to src. 
+// 6. I re-use the CardItems component by copying and pasting and setting each to its corresponding path.
